30b8c5696802e2d87540efb9f14549cb39b21210
Mylena Santos <mylenasantos@Mylenas-MacBook-Air.local>
2020-04-28 01:41:23 -0300

    carrousel film doing

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,22 +1,40 @@
 import React, { Component } from "react";
 import { View, Text } from 'react-native';
+import Carousel, { ParallaxImage } from 'react-native-snap-carousel';
 import { withNavigation } from 'react-navigation';
 import apiInterceptor from '../../services/apiInterceptor';
 import apiOMDB from '../../services/apiOMDB';
 import Navbar from "../../components/Navbar";
+import { Dimensions } from 'react-native';
 import styles from './styles';
 
-
+const { width: screenWidth } = Dimensions.get('window')
 class Main extends Component {
     state = {
-     films: [],
+     films: ['data'],
      apiKey: 'f2396906',
     };
 
     componentDidMount() {
-      this.handleFilms();
+      // this.handleFilms();
       
     }
+    _renderItem ({item, index}, parallaxProps) {
+      return (
+          <View style={styles.item}>
+              <ParallaxImage
+                  source={{ uri: item.thumbnail }}
+                  containerStyle={styles.imageContainer}
+                  style={styles.image}
+                  parallaxFactor={0.4}
+                  {...parallaxProps}
+              />
+              <Text style={styles.title} numberOfLines={2}>
+                  { item.title }
+              </Text>
+          </View>
+      );
+  }
 
     handleFilms = async () => {
      const response = await apiInterceptor.get('/exhibitions');
@@ -46,10 +64,20 @@ class Main extends Component {
         return (
          <View>
           <Navbar />
+          <View style={styles.filmExView}>
+           <Text style={styles.filmEx}>Filmes em cartaz</Text>
+          </View>
           {films && films.map((item) => {
             return (
             <View key={item.FilmId} style={styles.container}>
-              <Text style={styles.nameFilm}>{item.film.Name}</Text>
+              <Carousel
+                sliderWidth={screenWidth}
+                sliderHeight={screenWidth}
+                itemWidth={screenWidth - 60}
+                data={this.state.films}
+                renderItem={this._renderItem}
+                hasParallaxImages={true}
+            />
             </View>
             ) 
           })}
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -1,4 +1,5 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+const { width: screenWidth } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
    container: {
@@ -9,7 +10,31 @@ const styles = StyleSheet.create({
    nameFilm: {
       fontSize: 14,
       fontWeight: 'bold',
-   }
+   },
+   filmEx: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#da552f',
+   },
+   filmExView: {
+      marginTop: 40,
+      justifyContent: 'center',
+      flexDirection: 'row',
+   },
+   item: {
+      width: screenWidth - 60,
+      height: screenWidth - 60,
+    },
+    imageContainer: {
+      flex: 1,
+      marginBottom: Platform.select({ ios: 0, android: 1 }), // Prevent a random Android rendering issue
+      backgroundColor: 'white',
+      borderRadius: 8,
+    },
+    image: {
+      ...StyleSheet.absoluteFillObject,
+      resizeMode: 'cover',
+    },
 });
 
 export default styles;
